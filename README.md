f0eb32dd40ccc2c3ad2dfe1b3c2a949399b2e9a2
Austin Kurpuis <austin@bitovi.com>
2023-03-09 20:08:28 -0800

    Basic game working

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # lander
-A 2D simulation game where you program rocket boosters to land
+
+A 2D simulation game where you program rocket boosters to land.
+
+This is a work in progress and only supports keyboard control for now.
+
+## Develop
+
+1. Install dependencies `npm i`
+1. Start the dev server `npm start`
+1. Navigate to [http://localhost:3000](http://localhost:3000)
---
+++ modules/falcon-9.js
@@ -0,0 +1,118 @@
+export class Falcon9 {
+  constructor(game, {
+    color = "black",
+    width = 14,
+    height = 72,
+    position = {
+        x: game.canvas.width / 2,
+        y: getRandomInt(0, game.canvas.height / 4)
+    },
+    angle = getRandomInt(-1000, 1000) / 1000,
+    velocity = {
+        x: getRandomInt(-1000, 1000) / 1000,
+        y: getRandomInt(0, 1000) / 1000
+    },
+    thrust = 0.04,
+    fireBoosterEngine = false,
+    fireLeftThruster = false,
+    fireRightThruster = false,
+    maxLandingVelocity = 1
+  } = {}) {
+    this.game = game;
+    this.color = color;
+    this.width = width;
+    this.height = height;
+    this.position = position;
+    this.angle = angle;
+    this.velocity = velocity;
+    this.thrust = thrust;
+    this.fireBoosterEngine = fireBoosterEngine;
+    this.fireLeftThruster = fireLeftThruster;
+    this.fireRightThruster = fireRightThruster
+    this.maxLandingVelocity = maxLandingVelocity;
+
+    game.registerEntity(this);
+  }
+
+  render () {
+    this.game.context.save();
+    this.updatePosition();
+    this.drawShipBody();
+    this.drawEngineFlames();
+    this.game.context.restore();
+  }
+
+  updatePosition () {
+    const absoluteAngle = Math.abs(this.angle % (Math.PI * 2)) + (Math.PI / 2);
+
+    const minHeightSin = (this.width / 2) * Math.sin(this.angle < 0 && absoluteAngle < (Math.PI * 1.5) ? this.angle : -this.angle);
+    const minHeightCos = (this.height / 2) * Math.cos(this.angle);
+    const minHeight = absoluteAngle < Math.PI || absoluteAngle > (Math.PI * 2) ?
+      this.game.canvas.height - minHeightCos + minHeightSin:
+      this.game.canvas.height + minHeightCos + minHeightSin;
+    const positionX = this.position.x + this.velocity.x;
+    const positionY = Math.min(this.position.y + this.velocity.y, minHeight);
+    const bottom = positionY === minHeight;
+    
+    if(this.fireRightThruster) {
+      this.angle += Math.PI / 180;
+    } else if(this.fireLeftThruster) {
+      this.angle -= Math.PI / 180;
+    }
+
+    if(bottom) {
+      if (this.velocity.y || this.velocity.x) {
+        const combinedVelocity = this.velocity.y + this.velocity.x;
+        console.log("landing velocity", combinedVelocity);
+        console.log("landing angle", absoluteAngle);
+        const won = combinedVelocity < this.maxLandingVelocity;
+        const velocityMessage = ` Your landing velocity was ${combinedVelocity.toFixed(2)} and the max is ${this.maxLandingVelocity}.`
+        this.game.gameOver(`You ${won ? "won" : "lost"}!${!won ? velocityMessage : ""}`);
+      }
+
+      this.velocity.y = 0;
+      this.velocity.x = 0;
+    } else {
+      this.velocity.y += this.game.gravity;
+    }
+    if(this.fireBoosterEngine) {
+      this.velocity.x -= this.thrust * Math.sin(-this.angle);
+      this.velocity.y -= this.thrust * Math.cos(this.angle);
+    }
+    this.position.x = positionX;
+    this.position.y = positionY;
+  }
+
+  drawShipBody () {
+    const { context } = this.game;
+    context.beginPath();
+    context.translate(this.position.x, this.position.y);
+    context.rotate(this.angle);
+    context.rect(this.width * -0.5, this.height * -0.5, this.width, this.height);
+    context.fillStyle = this.color;
+    context.fill();
+    context.closePath();
+  }
+
+  drawEngineFlames() {
+    const { context } = this.game;
+    // Draw the flame if engine is on
+    if(this.fireBoosterEngine) {
+      context.beginPath();
+      context.moveTo(this.width * -0.5, this.height * 0.5);
+      context.lineTo(this.width * 0.5, this.height * 0.5);
+      context.lineTo(0, this.height * 0.5 + Math.random() * 10);
+      context.lineTo(this.width * -0.5, this.height * 0.5);
+      context.closePath();
+      context.fillStyle = "orange";
+      context.fill();
+    }
+  }
+}
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
